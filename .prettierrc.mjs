9f7a3eaed7dacccbf3d3c06b23ef870cb9a17605
GuillaumeLagouy1 <glagouy.pro@gmail.com>
2025-01-15 15:57:21 +0100

    Add an Astro Island with Svelte framework

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,11 +1,21 @@
 /** @type {import("prettier").Config} */
 export default {
-  plugins: ["prettier-plugin-astro"],
+  plugins: ["prettier-plugin-astro", "prettier-plugin-svelte"],
   overrides: [
     {
       files: "*.astro",
       options: {
         parser: "astro",
+        trailingComma: "es5",
+        tabWidth: 2,
+        semi: true,
+        singleQuote: true,
+      },
+    },
+    {
+      files: "*.svelte",
+      options: {
+        parser: "svelte",
         trailingComma: "es5",
         tabWidth: 2,
         semi: true,
